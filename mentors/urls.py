bb03aeba261c86ef94e011f6bda9c0471a5cf78e
Karambir Singh Nain <akarambir@gmail.com>
2013-03-04 02:08:44 +0530

    new mentee meeting applied

--- mentors/urls.py
+++ mentors/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib.auth.decorators import login_required
 
-from mentors.views import MenteeRecordsListView
+from mentors.views import MenteeRecordsListView, MenteeMeetingCreateView
 
 urlpatterns = patterns('',
     url(r'^$', login_required(MenteeRecordsListView.as_view()), name='mentors_home'),
+    url(r'^new-meeting/$', login_required(MenteeMeetingCreateView.as_view()), name='mentors_meeting'),
 )
--- mentors/views.py
+++ mentors/views.py
@@ -2,13 +2,19 @@ from django.http import HttpResponseRedirect
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 
 from django.contrib.auth.models import User
 from profiles.models import Faculty, Student
+from mentors.models import MenteeMeeting
+
 
 class MenteeRecordsListView(ListView):
     context_object_name = 'mentee_list'
     template_name = 'mentee_record_list.html'
     model = Student
+
+class MenteeMeetingCreateView(CreateView):
+    model = MenteeMeeting
+    template_name = 'mentee_meeting_create.html'
 
